7380bd74599bfcbd22ec7d6541d7b2575962e2f9
César Costa <cesarrcosta99@gmail.com>
2024-05-14 11:59:42 -0300

    Instalação do Biome(Padronização do Codigo)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
-import app from "./app"
+import app from './app'
 
-const PORT=process.env.PORT || 3001
+const PORT = process.env.PORT || 3001
 
-app.listen(PORT,'0.0.0.0')
-
+app.listen(PORT, '0.0.0.0')
